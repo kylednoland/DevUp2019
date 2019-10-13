7bfd4b9cd7f2f229ed98e336281a2593ab96ef11
Kyle Noland <kynoland@microsoft.com>
2019-10-13 16:33:37 -0500

    Initial Commit

---
+++ DevUp2019Test.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace DevUp2019Test.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(1 == 1);
+        }
+    }
+}
